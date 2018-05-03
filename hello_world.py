508be4569409fdb398f97c64cf966fb8dbadb992
root <root@localhost.localdomain>
2018-05-03 08:29:32 -0400

    msg

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
 	for item in range(1,11):
-		print('This is a Jenkins test! in CentOS!!! ' + str(item))
+		print('This is a Jenkins test! in CentOS!!! Fuck them all ' + str(item))
